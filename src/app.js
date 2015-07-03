9065499769542cbfce8cf34b1bb2b61a12c8d7fd
Christopher Roos <diverse@roosonline.dk>
2015-07-03 07:24:36 +0200

    forecast

--- src/app.js
+++ src/app.js
@@ -29,19 +29,7 @@ var curSlideItem = 0;
 var nextSlideItem = 1;
 var slideItems = [];
 
-// TIMERS AND INTERVALS
-  setInterval(function(){ 
-    locationObj.getLocation();
-    console.log('interval geolocation');
-  }, 60000);
-//////////////////////////
 
-Accel.init();
-Accel.on('tap', function(e) {
-  clt_1.text('Searching');
-  locationObj.getLocation();
-  console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
-});
 
 
 //Settings.option('group_names', undefined );
@@ -147,6 +135,56 @@ slideItems.push([
   {field: cwt_2, mainPos: { x: cwt_2.position().x, y: cwt_2.position().y }}
 ]);
 
+// FORECAST WEATHER ITEM
+var fwi_1 = new UI.Image({
+  position: new Vector2(10, 46),
+  size: new Vector2(40, 40),
+  image: 'images/01d.png',
+  compositing: 'set'
+});
+var fwi_2 = new UI.Image({
+  position: new Vector2(10, 86),
+  size: new Vector2(40, 40),
+  image: 'images/01d.png',
+  compositing: 'set'
+});
+var fwi_3 = new UI.Image({
+  position: new Vector2(10, 126),
+  size: new Vector2(40, 40),
+  image: 'images/01d.png',
+  compositing: 'set'
+});
+var fwt_1 = new UI.Text({
+  position: new Vector2(60, 48),
+  size: new Vector2(150, 40),
+  font: 'gothic-28',
+  text: '?°',
+  textAlign: 'left'
+});
+var fwt_2 = new UI.Text({
+  position: new Vector2(60, 88),
+  size: new Vector2(150, 40),
+  font: 'gothic-28',
+  text: '?°',
+  textAlign: 'left'
+});
+var fwt_3 = new UI.Text({
+  position: new Vector2(60, 128),
+  size: new Vector2(150, 40),
+  font: 'gothic-28',
+  text: '?°',
+  textAlign: 'left'
+});
+
+slideItems.push([
+  {showTime: false},
+  {field: fwi_1, mainPos: { x: fwi_1.position().x, y: fwi_1.position().y }},
+  {field: fwi_2, mainPos: { x: fwi_2.position().x, y: fwi_2.position().y }},
+  {field: fwi_3, mainPos: { x: fwi_3.position().x, y: fwi_3.position().y }},
+  {field: fwt_1, mainPos: { x: fwt_1.position().x, y: fwt_1.position().y }},
+  {field: fwt_2, mainPos: { x: fwt_2.position().x, y: fwt_2.position().y }},
+  {field: fwt_3, mainPos: { x: fwt_3.position().x, y: fwt_3.position().y }},
+]);
 
 var curSlides = require('slides');
 console.log(JSON.stringify(curSlides[0]));
@@ -159,6 +197,13 @@ main.add(clt_1); // CURRENT LOCAION ITEMS
 main.add(cwi_1); // CURRENT WEATHER ITEMS
 main.add(cwt_1); // CURRENT WEATHER ITEMS
 main.add(cwt_2); // CURRENT WEATHER ITEMS
+main.add(fwi_1); // FORECAST WEATHER ITEMS
+main.add(fwi_2); // FORECAST WEATHER ITEMS
+main.add(fwi_3); // FORECAST WEATHER ITEMS
+main.add(fwt_1); // FORECAST WEATHER ITEMS
+main.add(fwt_2); // FORECAST WEATHER ITEMS
+main.add(fwt_3); // FORECAST WEATHER ITEMS
+
 main.show();
 
 
@@ -230,6 +275,48 @@ function nextBottomSlide(){
   curSlideItem=nextSlideItem;
 }
 
+// weather object
+var weatherObj = weatherObj || {
+  getCurrent: function(){
+    //http://api.openweathermap.org/data/2.5/weather?q=Taastrup,dk&units=metric
+    //var callUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+locationinfo.locality.short_name+','+locationinfo.country.short_name+'&units='+units;
+    //var callUrl = 'http://api.openweathermap.org/data/2.5/find?lat=55.68225839999999&lon=12.5806846&cnt=1'+'&units='+units;
+    var callUrl = 'http://api.openweathermap.org/data/2.5/find?lat='+coordinates.latitude+'&lon='+coordinates.longitude+'&cnt=1&units='+units;
+    console.log(JSON.stringify(locationinfo));
+    ajax({ url: callUrl, type: 'json'},
+      function(data) {
+        data = data.list[0];
+        console.log('weather: '+callUrl);
+        console.log(JSON.stringify(data));
+        console.log('icon: '+data.weather[0].icon);
+        cwt_1.text(parseInt(data.main.temp)+'°');
+        var str = parseInt(data.wind.speed) + ' ' + windspeed + '\n'+data.main.humidity+' '+hum;
+        cwt_2.text(str);
+        cwi_1.image('images/'+data.weather[0].icon+'.png');
+    });
+    callUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+coordinates.latitude+'&lon='+coordinates.longitude+'&cnt=5&units='+units;
+    console.log(JSON.stringify(locationinfo));
+    ajax({ url: callUrl, type: 'json'},
+      function(data) {
+        console.log(JSON.stringify(data));
+        var d_names = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+        d_names = ['Søn', 'Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør'];
+        fwi_1.image('images/'+data.list[1].weather[0].icon+'.png');
+        fwi_2.image('images/'+data.list[2].weather[0].icon+'.png');
+        fwi_3.image('images/'+data.list[3].weather[0].icon+'.png');
+
+        var d = new Date(data.list[1].dt*1000);
+        fwt_1.text(parseInt(data.list[1].temp.day)+'°  '+d_names[d.getDay()] );
+        d = new Date(data.list[2].dt*1000);
+        fwt_2.text(parseInt(data.list[2].temp.day)+'°  '+d_names[d.getDay()]);
+        d = new Date(data.list[3].dt*1000);
+        fwt_3.text(parseInt(data.list[3].temp.day)+'°  '+d_names[d.getDay()]);
+    });    
+    
+  }
+};
+// End: weather object
+
 // currentLocation object
 var locationObj = locationObj || {
   getLocation: function(){
@@ -240,20 +327,32 @@ var locationObj = locationObj || {
     }    
   },
   locationSuccess: function(pos){
-    coordinates = pos.coords;
-    // http://maps.googleapis.com/maps/api/geocode/json?latlng=55.66268129528441,12.302745318828144
-    ajax({ url: 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+coordinates.latitude+','+coordinates.longitude, type: 'json' },
-      function(data) {
-        //console.log(JSON.stringify(data.results[0].formatted_address));       
-        locationinfo = {};
-        data.results[0].address_components.forEach(function (element, index) {
-          locationinfo[element.types[0]] = element;
-        });        
-        //console.log(JSON.stringify(locationinfo));
-        var locstr = locationinfo.route.short_name +' '+ locationinfo.street_number.short_name + '\n' + locationinfo.locality.short_name;      
-        clt_1.text(locstr);
-        weatherObj.getCurrent();
-    });   
+    console.log('coordinates != pos.coords');
+    console.log(JSON.stringify(coordinates));
+    console.log(JSON.stringify(pos.coords));
+    console.log(coordinates != pos.coords);
+    
+    // JS cannot compare two objects -- so stringify them first!
+    if ( JSON.stringify(coordinates) !== JSON.stringify(pos.coords) ) {
+      coordinates = pos.coords;
+      //coordinates = {latitude: '55.682401', longitude: '12.580773'};// Kbh
+      //coordinates = {latitude: '55.662363', longitude: '12.302768'};// Taastrup
+      // http://maps.googleapis.com/maps/api/geocode/json?latlng=55.66268129528441,12.302745318828144
+      ajax({ url: 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+coordinates.latitude+','+coordinates.longitude, type: 'json' },
+        function(data) {
+          //console.log(JSON.stringify(data.results[0].formatted_address));       
+          locationinfo = {latitude: coordinates.latitude, longitude: coordinates.longitude};
+          data.results[0].address_components.forEach(function (element, index) {
+            locationinfo[element.types[0]] = element;
+          });        
+          //console.log(JSON.stringify(locationinfo));
+          var locstr = locationinfo.route.short_name +' '+ locationinfo.street_number.short_name + '\n' + locationinfo.locality.short_name;      
+          clt_1.text(locstr);
+          weatherObj.getCurrent();
+      });
+    }else{
+      console.log('Same Location');
+    }
   },
   locationError: function(err){
     console.warn('location error (' + err.code + '): ' + err.message);
@@ -268,26 +367,9 @@ locationObj.getLocation();
 
 // End: Watch Location
 
-// weather object
-var weatherObj = weatherObj || {
-  getCurrent: function(){
-    //http://api.openweathermap.org/data/2.5/weather?q=Taastrup,dk&units=metric
-    var callUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+locationinfo.locality.long_name+','+locationinfo.country.short_name+'&units='+units;
-    ajax({ url: callUrl, type: 'json'},
-      function(data) {
-        console.log('weather:'+callUrl);
-        console.log(JSON.stringify(data));
-        console.log('icon: '+data.weather[0].icon);
-        cwt_1.text(parseInt(data.main.temp)+'°');
-        var str = parseInt(data.wind.speed) + ' ' + windspeed + '\n'+data.main.humidity+' '+hum;
-        cwt_2.text(str);
-        cwi_1.image('images/'+data.weather[0].icon+'.png');
-    });
-  }
-};
-// End: weather object
 
 
+//var hueSelectLightsObj = require('hue.js').hueSelectLightsObj;
 var hueSelectLightsObj = hueSelectLightsObj || {
     init: function(){
     this.Menu = new UI.Menu({
@@ -300,26 +382,26 @@ var hueSelectLightsObj = hueSelectLightsObj || {
     });
       
     this.Menu.on('select', function(e) {
-      console.log('hueSelectLightsObj > self.selectItem(e)');
+      //console.log('hueSelectLightsObj > self.selectItem(e)');
       this.selectItem(e);
     }.bind(this));
       
     this.Menu.on('hide', function(e){
       var newGroup = {name:"Group1", lights: []};
       for (var key in this.allLights){
-        console.log(JSON.stringify(this.allLights[key]));
+        //console.log(JSON.stringify(this.allLights[key]));
         if (this.allLights[key].added){
-          console.log('added :)');
+          //console.log('added :)');
           newGroup.lights.push( this.allLights[key].lightNb );
         }
       }
-      console.log(JSON.stringify(newGroup));
+      //console.log(JSON.stringify(newGroup));
       newGroup.name = "Lights: "+ newGroup.lights.join();
       var callUrl = hueApiCall+'/groups/';
       ajax({ url: callUrl, method: 'post', data:newGroup, type: 'json'},
         function(data) {
-          console.log(JSON.stringify(data));
-          hueMenuObj.updateSections();
+          //console.log(JSON.stringify(data));
+          hueMenuObj.updateSections(); // Hvad gør vi her?
         });
     }.bind(this));
       
@@ -335,7 +417,7 @@ var hueSelectLightsObj = hueSelectLightsObj || {
     //var callUrl = hueApiCall+'/lights/';
     ajax({ url: hueApiCall+'/lights/', method: 'get', type: 'json'},
         function(data) {
-          console.log(JSON.stringify(data));
+          //console.log(JSON.stringify(data));
           for (var key in data) {
             var curLight = {
               title: decodeURIComponent(data[key].name),
@@ -344,7 +426,7 @@ var hueSelectLightsObj = hueSelectLightsObj || {
               lightNb: key,
               added: false,
             };
-            console.log(JSON.stringify(curLight));
+            //console.log(JSON.stringify(curLight));
             this.allLights.push(curLight);
           }
           this.Menu.items(0, this.allLights);
@@ -359,7 +441,7 @@ var hueSelectLightsObj = hueSelectLightsObj || {
       //  function(data) {
       //    console.log(JSON.stringify(data));
       //  });
-      console.log(JSON.stringify(e.item));
+      //console.log(JSON.stringify(e.item));
       if (e.item.added){
         this.Menu.item(e.sectionIndex, e.itemIndex, { icon: 'images/pebble-checkmark-off.png', added: false } );
       }else{
@@ -368,6 +450,8 @@ var hueSelectLightsObj = hueSelectLightsObj || {
     }
   }
 };
+
+// var hueMenuObj = require('hue.js').hueMenuObj;
 var hueMenuObj = hueMenuObj || {
   init: function(){
     var self = this;
@@ -380,7 +464,7 @@ var hueMenuObj = hueMenuObj || {
     });
     
     this.Menu.on('select', function(e) {
-      console.log('hueMenuObj > self.selectItem(e)');
+      //console.log('hueMenuObj > self.selectItem(e)');
       this.selectItem(e);
     }.bind(this));
     
@@ -389,7 +473,7 @@ var hueMenuObj = hueMenuObj || {
       // Delete current group (groupID);
       ajax({ url: hueApiCall+'/groups/'+e.item.groupID+'/', method: 'delete', type: 'json'},
         function(data) {
-          console.log(JSON.stringify(data));
+          //console.log(JSON.stringify(data));
           this.updateSections();
         }.bind(this));
     }.bind(this));
@@ -407,16 +491,16 @@ var hueMenuObj = hueMenuObj || {
     ajax({ url: hueApiCall+'/groups/', method: 'get', type: 'json'},
          function(data){
            for (var key in data){
-             console.log(JSON.stringify(data[key]));           
+             //console.log(JSON.stringify(data[key]));           
              var thisGroup = {
                title: data[key].name,
                subtitle: (data[key].action.on ? "Turn Off" : "Turn On"),
                on: data[key].action.on,
                groupID: key,
                action: function(e){
-                 console.log(e.item.on);
+                 //console.log(e.item.on);
                  var nextState = (e.item.on) ? false : true;
-                 console.log(nextState);
+                 //console.log(nextState);
                  this.Menu.item(e.sectionIndex, e.itemIndex, { on: nextState, subtitle: (nextState ? "Turn Off" : "Turn On") } );
                  ajax({ url: hueApiCall+'/groups/'+e.item.groupID+'/action', data:{on: nextState, "bri":150,"sat":100,"hue":2000}, method: 'put', type: 'json'},
                       function(data) {
@@ -425,10 +509,10 @@ var hueMenuObj = hueMenuObj || {
              };
              this.allLights.push(thisGroup);
            }
-           console.log('hueMenuObj.allLights');
-           console.log(JSON.stringify(this.allLights));
-           console.log(JSON.stringify(this.allLights[this.allLights.length-1]));
-           console.log(this.allLights.length-1);
+           //console.log('hueMenuObj.allLights');
+           //console.log(JSON.stringify(this.allLights));
+           //console.log(JSON.stringify(this.allLights[this.allLights.length-1]));
+           //console.log(this.allLights.length-1);
            this.Menu.items(0, this.allLights);
          }.bind(this));
     this.Menu.items(0, this.allLights);
@@ -538,4 +622,18 @@ main.on('click', 'select', function(e) {
 
 main.on('click', 'down', function(e) {
   nextBottomSlide();
+});
+
+// TIMERS AND INTERVALS
+  setInterval(function(){ 
+    locationObj.getLocation();
+    console.log('interval geolocation');
+  }, 60000);
+//////////////////////////
+
+Accel.init();
+Accel.on('tap', function(e) {
+  //clt_1.text('Searching');
+  locationObj.getLocation();
+  console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
 });
--- src/hue.js
+++ src/hue.js
@@ -1,11 +1,13 @@
 /* jshint undef: true, unused: false */
 /*global require, module */
 var UI = require('ui');
+var ajax = require('ajax');
 
 var hueIP = '192.168.0.18';
 var hueUser = 'newdeveloper';
 var hueApiCall = 'http://'+hueIP+'/api/'+hueUser;
 
+  
 module.exports.hueSelectLightsObj = {
     init: function(){
     this.Menu = new UI.Menu({
@@ -16,30 +18,31 @@ module.exports.hueSelectLightsObj = {
       compositing: 'set',
       sections: []
     });
+      
     this.Menu.on('select', function(e) {
-      hueSelectLightsObj.selectItem(e);
-    });
+      //console.log('hueSelectLightsObj > self.selectItem(e)');
+      this.selectItem(e);
+    }.bind(this));
+      
     this.Menu.on('hide', function(e){
       var newGroup = {name:"Group1", lights: []};
-      for (var key in hueSelectLightsObj.allLights){
-        console.log(JSON.stringify(hueSelectLightsObj.allLights[key]));
-        if (hueSelectLightsObj.allLights[key].added){
-          console.log('added :)');
-          newGroup.lights.push( hueSelectLightsObj.allLights[key].lightNb );
+      for (var key in this.allLights){
+        //console.log(JSON.stringify(this.allLights[key]));
+        if (this.allLights[key].added){
+          //console.log('added :)');
+          newGroup.lights.push( this.allLights[key].lightNb );
         }
       }
-      
-      console.log(JSON.stringify(newGroup));
-      
+      //console.log(JSON.stringify(newGroup));
       newGroup.name = "Lights: "+ newGroup.lights.join();
       var callUrl = hueApiCall+'/groups/';
       ajax({ url: callUrl, method: 'post', data:newGroup, type: 'json'},
         function(data) {
-          console.log(JSON.stringify(data));
-          hueMenuObj.updateSections();
+          //console.log(JSON.stringify(data));
+          hueMenuObj.updateSections(); // Hvad gør vi her?
         });
-
-    });
+    }.bind(this));
+      
     this.updateSections();
     this.Menu.show();
   },
@@ -50,9 +53,12 @@ module.exports.hueSelectLightsObj = {
         subtitle: 'Set levels for lights'
       }];
     //var callUrl = hueApiCall+'/lights/';
+    console.log('hueApiCall+/lights/');
+    console.log(hueApiCall+'/lights/');
+
     ajax({ url: hueApiCall+'/lights/', method: 'get', type: 'json'},
         function(data) {
-          console.log(JSON.stringify(data));
+          //console.log(JSON.stringify(data));
           for (var key in data) {
             var curLight = {
               title: decodeURIComponent(data[key].name),
@@ -61,11 +67,11 @@ module.exports.hueSelectLightsObj = {
               lightNb: key,
               added: false,
             };
-            console.log(JSON.stringify(curLight));
-            hueSelectLightsObj.allLights.push(curLight);
+            //console.log(JSON.stringify(curLight));
+            this.allLights.push(curLight);
           }
-          hueSelectLightsObj.Menu.items(0, hueSelectLightsObj.allLights);
-        });
+          this.Menu.items(0, this.allLights);
+        }.bind(this));
     
     this.Menu.items(0, this.allLights);
   },
@@ -76,17 +82,19 @@ module.exports.hueSelectLightsObj = {
       //  function(data) {
       //    console.log(JSON.stringify(data));
       //  });
-      console.log(JSON.stringify(e.item));
+      //console.log(JSON.stringify(e.item));
       if (e.item.added){
-        hueSelectLightsObj.Menu.item(e.sectionIndex, e.itemIndex, { icon: 'images/pebble-checkmark-off.png', added: false } );
+        this.Menu.item(e.sectionIndex, e.itemIndex, { icon: 'images/pebble-checkmark-off.png', added: false } );
       }else{
-        hueSelectLightsObj.Menu.item(e.sectionIndex, e.itemIndex, { icon: 'images/pebble-checkmark-on.png', added: true } );
+        this.Menu.item(e.sectionIndex, e.itemIndex, { icon: 'images/pebble-checkmark-on.png', added: true } );
       }      
     }
   }
 };
+
 module.exports.hueMenuObj = {
   init: function(){
+    var self = this;
     this.Menu = new UI.Menu({
       backgroundColor: 'black',
       textColor: 'blue',
@@ -94,18 +102,22 @@ module.exports.hueMenuObj = {
       highlightTextColor: 'black',
       sections: []
     });
+    
     this.Menu.on('select', function(e) {
-      hueMenuObj.selectItem(e);
-    });
+      //console.log('hueMenuObj > self.selectItem(e)');
+      this.selectItem(e);
+    }.bind(this));
+    
     this.Menu.on('longSelect', function(e){
       console.log(JSON.stringify(e.item));
       // Delete current group (groupID);
       ajax({ url: hueApiCall+'/groups/'+e.item.groupID+'/', method: 'delete', type: 'json'},
         function(data) {
-          console.log(JSON.stringify(data));
-          hueMenuObj.updateSections();
-        });
-    });
+          //console.log(JSON.stringify(data));
+          this.updateSections();
+        }.bind(this));
+    }.bind(this));
+    
     this.updateSections();
     this.Menu.show();
   },
@@ -114,55 +126,40 @@ module.exports.hueMenuObj = {
         title: 'Add Group',
         //icon: 'images/menu_icon.png',
         subtitle: 'Select lights',
-        action: function(){ hueSelectLightsObj.init(); }
+        action: function(){ hueSelectLightsObj.init(); } // Hvad gør vi her?
       }];
+    console.log('hueApiCall+/groups/');
+    console.log(hueApiCall+'/groups/');
     ajax({ url: hueApiCall+'/groups/', method: 'get', type: 'json'},
          function(data){
            for (var key in data){
-             console.log(JSON.stringify(data[key]));           
+             //console.log(JSON.stringify(data[key]));           
              var thisGroup = {
                title: data[key].name,
                subtitle: (data[key].action.on ? "Turn Off" : "Turn On"),
                on: data[key].action.on,
                groupID: key,
                action: function(e){
-                 console.log(e.item.on);
+                 //console.log(e.item.on);
                  var nextState = (e.item.on) ? false : true;
-                 console.log(nextState);
-                 hueMenuObj.Menu.item(e.sectionIndex, e.itemIndex, { on: nextState, subtitle: (nextState ? "Turn Off" : "Turn On") } );
+                 //console.log(nextState);
+                 this.Menu.item(e.sectionIndex, e.itemIndex, { on: nextState, subtitle: (nextState ? "Turn Off" : "Turn On") } );
                  ajax({ url: hueApiCall+'/groups/'+e.item.groupID+'/action', data:{on: nextState, "bri":150,"sat":100,"hue":2000}, method: 'put', type: 'json'},
                       function(data) {
                       });
-               }
+               }.bind(this)
              };
-             hueMenuObj.allLights.push(thisGroup);
+             this.allLights.push(thisGroup);
            }
-           console.log('hueMenuObj.allLights');
-           console.log(JSON.stringify(hueMenuObj.allLights));
-           console.log(JSON.stringify(hueMenuObj.allLights[hueMenuObj.allLights.length-1]));
-           console.log(hueMenuObj.allLights.length-1);
-           hueMenuObj.Menu.items(0, hueMenuObj.allLights);
-         });
+           //console.log('hueMenuObj.allLights');
+           //console.log(JSON.stringify(this.allLights));
+           //console.log(JSON.stringify(this.allLights[this.allLights.length-1]));
+           //console.log(this.allLights.length-1);
+           this.Menu.items(0, this.allLights);
+         }.bind(this));
     this.Menu.items(0, this.allLights);
   },
   selectItem: function(e){
     e.item.action(e);
-  }
-};
-
-module.exports.hueGroupSettingObj = {
-    init: function(){
-    this.Menu = new UI.Menu({
-      backgroundColor: 'black',
-      textColor: 'blue',
-      highlightBackgroundColor: 'blue',
-      highlightTextColor: 'black',
-      sections: []
-    });
-    this.Menu.on('select', function(e) {
-      hueMenuObj.selectItem(e);
-    });
-    this.updateSections();
-    this.Menu.show();
   }
 };
